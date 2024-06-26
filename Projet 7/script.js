74b2a6d1eb82e09793f059445ea1b29d71fe8f7e
Genevieve Ndhomou <ndhomou@gmail.com>
2024-06-26 10:50:14 +0100

    Projet 7

---
+++ Projet 7/script.js
@@ -0,0 +1,9 @@
+const left = document.querySelector('.xbox1')
+const right = document.querySelector('.xbox2')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
